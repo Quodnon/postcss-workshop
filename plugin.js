8060f6b2885294996e96cfaf203b60cb0355287f
quodnon <feanorfeanturi@gmail.com>
2016-05-28 11:57:48 +0000

    alex rewrite

--- plugin.js
+++ plugin.js
@@ -1,61 +1,71 @@
-"use strict";
+"use strict"
 
-const postcss = require("postcss");
+const postcss = require("postcss")
+const name = "postcss-short-supports"
+// const {name} = require("./package")
 
-module.exports =postcss.plugin("postcss-short-supports",()=>css=>{
+module.exports = postcss.plugin(name, options => css =>
+{
+    let {except = /.^/} = options || {}
+
     css.walkDecls("background-attachment", decl =>
     {
-        if (decl.value !='fixed') return
-        
+        if (decl.value != "fixed") return
+
         let {parent} = decl
-        
-        let hasPosition = parent.some(node=>
+        let {selector} = parent
+
+        if (except.test(selector)) return
+
+        let hasPosition = parent.some(node =>
         {
-            if (node.type !='decl') return
-            if (node.prop !='position') return
-            if (node.value =='static') return true
+            if (node.type != "decl") return
+            if (node.prop != "position") return
+            if (node.value != "static") return true
         })
-        
-        if (!hasPosition){
-            let decl = postcss.decl({ prop: "position", value:"relative"})
-            parent.append(decl)
-        }
-        
-        let zIndex = parent.some(node=>{
-            return node.type=="decl" && node.prop=='z-index'
-        })
-        
-        if(!zIndex){
-            parent.append({ prop:"z-index", value:0 })
-        }
-        
-        let overflow = parent.nodes.find(node=>
+
+        if (!hasPosition)
         {
-            return node.type =='decl'
-                && node.prop =='overflow'
+            parent.append({ prop: "position", value: "relative" })
+        }
+
+        let zIndex = parent.some(node =>
+        {
+            return node.type == "decl"
+                && node.prop == "z-index"
         })
-        
-        if (overflow) overflow.value ='hidden'
-        else parent.append({prop:'overflow',value:'hidden'})
-        
-        parent.append({prop:"background-size", value:"0 0"})
+
+        if (!zIndex)
+        {
+            parent.append({ prop: "z-index", value: 0 })
+        }
+
+        let overflow = parent.nodes.find(node =>
+        {
+            return node.type == "decl"
+                && node.prop == "overflow"
+        })
+
+        if (overflow) overflow.value = "hidden"
+        else parent.append({ prop: "overflow", value: "hidden" })
+
+        parent.append({ prop: "background", value: "none" })
         decl.remove()
-        
-        let selector =`${parent.selector}::before`
-        
-        css.append(
-            `
-                ${selector}
-                {
-                    content:"";
-                    position: fixed;
-                    z-index: -1;
-                    will-change: transform;
-                    
-                    top:0; left:0;
-                    width:100%; height: 100%;
-                    background-image:inherit;
-                }
-            `)
+
+        css.append
+        (`
+            ${selector}::before
+            {
+                content: "";
+                position: fixed;
+                z-index: -1;
+                will-change: transform;
+
+                top: 0; left: 0;
+                width: 100%; height: 100%;
+                background-image: inherit;
+                background-size: inherit;
+            }
+        `)
     })
-});
+})
