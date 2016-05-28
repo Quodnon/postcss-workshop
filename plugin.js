131d0f115c12e03d363bea2bed2abed7c98d2830
quodnon <feanorfeanturi@gmail.com>
2016-05-28 11:04:13 +0000

    first plugin end

--- plugin.js
+++ plugin.js
@@ -3,9 +3,23 @@
 const postcss = require("postcss");
 
 module.exports =postcss.plugin("postcss-short-supports",()=>css=>{
-    css.walkAtRules("supports",supports =>
+    css.walkDecls("background-attachment", decl =>
     {
-        supports.params=supports.params.replace(/[-\w]+(!?\:)/g, "($&: initial)")
+        if (decl.value !='fixed') return
         
+        let {parent} = decl
+        
+        let hasPosition = parent.some(node=>
+        {
+            if (node.type !='decl') return
+            if (node.prop !='position') return
+            if (node.value =='static') return true
+        })
+        
+        if (!hasPosition)
+        {
+            let decl = postcss.decl({ prop: "position", value:"relative"})
+            parent.append(decl)
+        }
     })
 });
